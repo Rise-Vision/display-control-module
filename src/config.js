e8e52bff6792a15e54c36b67049003f4a9f99e84
santiago <santiago@nul-unu.com>
2017-11-30 12:48:45 -0500

    move logging constants to config file

--- src/config.js
+++ src/config.js
@@ -1,3 +1,12 @@
+const common = require("common-display-module");
+
+const moduleName = "display-control"
+
 module.exports = {
-  moduleName: "display-control"
+  bqProjectName: "client-side-events",
+  bqDataset: "Module_Events",
+  bqTable: "display_control_events",
+  failedEntryFile: "display-control-failed.log",
+  logFolder: common.getModulePath(moduleName),
+  moduleName
 }
--- src/logger.js
+++ src/logger.js
@@ -1,16 +1,10 @@
 /* eslint-disable space-in-parens */
 const common = require("common-display-module");
-const config = require("./config");
-const moduleName = config.moduleName;
+const {
+  bqProjectName, bqDataset, bqTable, failedEntryFile, logFolder, moduleName
+} = require("./config");
 
-const BQ_PROJECT_NAME = "client-side-events"
-const BQ_DATASET = "Module_Events"
-const BQ_TABLE = "display_control_events"
-
-const logFolder = common.getModulePath(moduleName)
-const FAILED_ENTRY_FILE = "display-control-failed.log"
-
-const externalLogger = require("common-display-module/external-logger")(BQ_PROJECT_NAME, BQ_DATASET, FAILED_ENTRY_FILE);
+const externalLogger = require("common-display-module/external-logger")(bqProjectName, bqDataset, failedEntryFile);
 const logger = require("rise-common-electron/logger")(externalLogger, logFolder, moduleName);
 
 // Creates the detail data structure that the logging functions expect.
@@ -30,7 +24,7 @@ function debug(message) {
 
 function error(eventDetails, userFriendlyMessage) {
   detailsFor(eventDetails, {})
-  .then(detail => logger.error(detail, userFriendlyMessage, BQ_TABLE));
+  .then(detail => logger.error(detail, userFriendlyMessage, bqTable));
 }
 
 module.exports = {debug, error};
