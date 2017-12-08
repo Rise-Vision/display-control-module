628190a5483e45c2f88f397e59e2f25d75ea000a
santiago <santiago@nul-unu.com>
2017-12-08 13:43:46 -0600

    parse control configuration content

---
+++ src/parser.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-extra-parens */
+
+// This module interprets the syntax of the Display Control Configuration File
+// as mentioned in the design document.
+
+function parseContent(content) {
+  if (!content) {
+    return null;
+  }
+
+  return (content
+    .split("\n")
+    .filter(line => line.includes('='))
+    .map(line => line.split('='))
+    .reduce((settings, [key, value]) =>
+      Object.assign(settings, {[key]: value}), {}
+    )
+  );
+}
+
+module.exports = {parseContent};
