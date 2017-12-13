ba7b7e1ccbce9755271da6f97ee47cf68b82d99f
santiago <santiago@nul-unu.com>
2017-12-13 10:36:50 -0600

    add integration tests for RS-232

--- test/integration/screen-rs232.js
+++ test/integration/screen-rs232.js
@@ -2,44 +2,51 @@
 /* eslint-disable max-statements, no-magic-numbers */
 const assert = require("assert");
 const common = require("common-display-module");
-const child = require("child_process");
 const simple = require("simple-mock");
+const SerialPort = require('serialport/test');
+const MockBinding = SerialPort.Binding;
 
 const config = require("../../src/config");
 const screen = require("../../src/screen");
 
-const cec = require("../../src/strategies/cec");
-const CECControlStrategy = require("../../src/strategies/cec/strategy");
+const rs232 = require("../../src/strategies/rs232");
+const SerialPortFactory = require("../../src/strategies/rs232/serial-port-factory");
 
-describe("Screen - Integration", () =>
-{
+describe("Screen RS232 - Integration", () => {
 
-  beforeEach(() =>
-  {
+  beforeEach(() => {
     simple.mock(common, "connect").resolveWith({});
     simple.mock(common, "getDisplayId").resolveWith("ABC");
     simple.mock(common, "broadcastMessage").returnWith();
 
-    config.setDisplayControlSettings({interface: "CEC"});
+    simple.mock(SerialPortFactory, "create").callFn(path =>
+      new SerialPort(path)
+    )
+
+    MockBinding.createPort("/dev/mock", {echo: false, record: true});
+
+    config.setDisplayControlSettings({
+      interface: "RS232",
+      "serial-port": "/dev/mock",
+      "serial-screen-on-cmd": "01 30 41 30 41 30 43 02 43 32 30 33 44 36 30 30 30 31 03 73 0d",
+      "serial-screen-off-cmd": "01 30 41 30 41 30 43 02 43 32 30 33 44 36 30 30 30 34 03 76 0d"
+    });
   });
 
-  afterEach(()=>
+  afterEach(done=>
   {
     simple.restore();
     config.setDisplayControlSettings(null);
-    cec.clear();
+
+    rs232.clear().then(() => {
+      MockBinding.reset();
+
+      done();
+    });
   });
 
-  it("should turn on the screen using CEC commands", done =>
+  it("should turn on the screen using RS232 commands", done =>
   {
-    // no error
-    simple.mock(child, "exec").callFn((path, callback) => callback(false));
-
-    simple.mock(cec, "init").resolveWith(new CECControlStrategy(
-    {
-      WriteRawMessage: () => Promise.resolve()
-    }));
-
     screen.turnOn()
     .then(() =>
     {
@@ -63,7 +70,7 @@ describe("Screen - Integration", () =>
       // the BigQuery row entry, see design doc for individual element description
       const row = data.data;
       assert.equal(row.event, "turn-screen-on");
-      assert.equal(row.event_details, "on 0");
+      assert.equal(row.event_details, "01 30 41 30 41 30 43 02 43 32 30 33 44 36 30 30 30 31 03 73 0d");
       assert.equal(row.display_id, "ABC");
 
       done();
@@ -76,16 +83,8 @@ describe("Screen - Integration", () =>
     });
   });
 
-  it("should turn off the screen using CEC commands", done =>
+  it("should turn off the screen using RS-232 commands", done =>
   {
-    // no error
-    simple.mock(child, "exec").callFn((path, callback) => callback(false));
-
-    simple.mock(cec, "init").resolveWith(new CECControlStrategy(
-    {
-      WriteRawMessage: () => Promise.resolve()
-    }));
-
     screen.turnOff()
     .then(() =>
     {
@@ -109,7 +108,7 @@ describe("Screen - Integration", () =>
       // the BigQuery row entry, see design doc for individual element description
       const row = data.data;
       assert.equal(row.event, "turn-screen-off");
-      assert.equal(row.event_details, "standby 0");
+      assert.equal(row.event_details, "01 30 41 30 41 30 43 02 43 32 30 33 44 36 30 30 30 34 03 76 0d");
       assert.equal(row.display_id, "ABC");
 
       done();
@@ -122,59 +121,15 @@ describe("Screen - Integration", () =>
     });
   });
 
-  it("should log error if cec-utils are not installed", (done) =>
-  {
-    // cec-utils not found
-    simple.mock(child, "exec").callFn((path, callback) => callback(true));
+  it("should log error if command execution fails", (done) => {
+    simple.mock(SerialPortFactory, "create").callFn(path => {
+      const port = new SerialPort(path);
 
-    simple.mock(cec, "init").callFn(cec.checkCecUtilsConfigured);
+      port.write = (data, errorCallback) =>
+        errorCallback({message: 'display not available'});
 
-    // either turnOn() or turnOff() will fail
-    screen.turnOff()
-    .then(() =>
-    {
-      // should have resulted in a call to logging module
-      assert(common.broadcastMessage.called);
-
-      // this is the actual event object sent to the logging module
-      const event = common.broadcastMessage.lastCall.args[0];
-
-      // I sent the event
-      assert.equal(event.from, "display-control");
-      // it's a log event
-      assert.equal(event.topic, "log");
-
-      const data = event.data
-      assert.equal(data.projectName, "client-side-events");
-      assert.equal(data.datasetName, "Module_Events");
-      assert.equal(data.table, "display_control_events");
-      assert.equal(data.failedEntryFile, "display-control-failed.log");
-
-      // the BigQuery row entry, see design doc for individual element description
-      const row = data.data;
-      assert.equal(row.event, "error");
-      assert.equal(row.event_details, "cec-utils not installed in Operating System");
-      assert.equal(row.display_id, "ABC");
-
-      done();
-    })
-    .catch(error =>
-    {
-      assert.fail(error);
-
-      done();
+      return port;
     });
-  });
-
-  it("should log error if command execution fails", (done) =>
-  {
-    // cec-utils found
-    simple.mock(child, "exec").callFn((path, callback) => callback(false));
-
-    simple.mock(cec, "init").resolveWith(new CECControlStrategy(
-    {
-      WriteRawMessage: () => Promise.reject(Error('display not available'))
-    }));
 
     // either turnOn() or turnOff() will fail
     screen.turnOff()
@@ -202,7 +157,7 @@ describe("Screen - Integration", () =>
         // the BigQuery row entry, see design doc for individual element description
         const row = data.data;
         assert.equal(row.event, "turn-screen-off");
-        assert.equal(row.event_details, "standby 0");
+        assert.equal(row.event_details, "01 30 41 30 41 30 43 02 43 32 30 33 44 36 30 30 30 34 03 76 0d");
         assert.equal(row.display_id, "ABC");
       }
 
