c74919d70068f9bb02ffac7c3f58b30743f05a3a
Stuart Lees <stuartlees@gmail.com>
2018-01-08 16:05:19 -0500

    Copy node_modules from separate folder into webpack output directory

--- post-build.sh
+++ post-build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 set -x
-ARCHS=(x32 x64 armv7l)
+ARCHS=(32 64 armv7l)
 
 for ARCH in "${ARCHS[@]}"
 do
   if [ $ARCH == "armv7l" ]
   then
     cp build-armv7l/*.sh build
+    rm -rf build-x-armv7l
   else
     cp build-${ARCH}/*.{sh,exe} build
+    rm -rf build-x${ARCH}
   fi
   rm -rf build-${ARCH}
 done
--- pre-build.sh
+++ pre-build.sh
@@ -8,15 +8,15 @@ cd ./build-temp
 npm install --production
 cd ..
 
-ARCHS=(x32 x64 armv7l)
+ARCHS=(x32 x64 x-armv7l)
 
 for ARCH in "${ARCHS[@]}"
 do
   rm -rf build-${ARCH}
-  mkdir -p build-${ARCH}/display-control
-  cp ./build-temp/package.json ./build-${ARCH}/display-control
-  cp -r ./build-temp/node_modules ./build-${ARCH}/display-control
-  "$(npm bin)"/electron-rebuild --version 1.7.8 --arch ${ARCH} --only serialport --module-dir ./build-${ARCH}/display-control
+  mkdir -p build-${ARCH}
+  cp ./build-temp/package.json ./build-${ARCH}
+  cp -r ./build-temp/node_modules ./build-${ARCH}
+  "$(npm bin)"/electron-rebuild --version 1.7.8 --arch ${ARCH} --only serialport --module-dir ./build-${ARCH}
 done
 rm -rf build
 mkdir -p build
