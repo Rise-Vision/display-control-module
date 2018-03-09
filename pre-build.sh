721576b5a53b94d46bc4a8d5e3dc62e207c80c99
Stuart Lees <stuartlees@gmail.com>
2018-03-09 15:36:44 -0500

    Update pre-build script to not manage serialport bin/

--- pre-build.sh
+++ pre-build.sh
@@ -20,18 +20,14 @@ do
     mkdir -p build-${PLATFORM}-${ARCH}
     cp ./build-temp/package.json ./build-${PLATFORM}-${ARCH}
     cp -r ./build-temp/node_modules ./build-${PLATFORM}-${ARCH}
-    rm -rf ./build-${PLATFORM}-${ARCH}/node_modules/serialport/bin
     rm -rf ./build-${PLATFORM}-${ARCH}/node_modules/serialport/build/Release
     if [ $PLATFORM == "lnx" ] && [ $ARCH == "x32" ]
     then
-      cp -r ./serialport/lnx/ia32/bin ./build-${PLATFORM}-${ARCH}/node_modules/serialport
       cp -r ./serialport/lnx/ia32/build/Release ./build-${PLATFORM}-${ARCH}/node_modules/serialport/build
     elif [ $PLATFORM == "win" ] && [ $ARCH == "x32" ]
     then
-      cp -r ./serialport/win/ia32/bin ./build-${PLATFORM}-${ARCH}/node_modules/serialport
       cp -r ./serialport/win/ia32/build/Release ./build-${PLATFORM}-${ARCH}/node_modules/serialport/build
     else
-      cp -r ./serialport/${PLATFORM}/${ARCH}/bin ./build-${PLATFORM}-${ARCH}/node_modules/serialport
       cp -r ./serialport/${PLATFORM}/${ARCH}/build/Release ./build-${PLATFORM}-${ARCH}/node_modules/serialport/build
     fi
   done
